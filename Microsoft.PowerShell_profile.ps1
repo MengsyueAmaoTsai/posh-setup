dbd393ba6f23bd030d7035c3e4832dde44fff4a5
Mengsyue Amao Tsai <88216325+MengsyueAmaoTsai@users.noreply.github.com>
2023-11-02 15:14:09 +0800

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -330,4 +330,4 @@ function which ($command) {
 
 Import-Module posh-git
 Import-Module -Name Terminal-Icons
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\easy-term.omp.json"  | Invoke-Expression
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\mengs.omp.json"  | Invoke-Expression
